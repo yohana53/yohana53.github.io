612727167697119840818f4fdedcfb34ef1385f3
yohana53 <yohanafashion53@gmail.com>
2026-03-06 19:54:13 +0200

    Update products.js

---
+++ products.js
@@ -0,0 +1,51 @@
+const products = [
+
+{
+id:1,
+name:"Classic T-Shirt",
+price:15000,
+category:"tshirts",
+rating:4,
+images:[
+"images/tshirt1.jpg",
+"images/tshirt2.jpg"
+]
+},
+
+{
+id:2,
+name:"Elegant Dress",
+price:45000,
+category:"dresses",
+rating:5,
+images:[
+"images/dress1.jpg",
+"images/dress2.jpg"
+]
+},
+
+{
+id:3,
+name:"Running Shoes",
+price:65000,
+category:"shoes",
+rating:4,
+images:[
+"images/shoes1.jpg",
+"images/shoes2.jpg"
+]
+},
+
+{
+id:4,
+name:"Women Bag",
+price:35000,
+category:"bags",
+rating:5,
+images:[
+"images/bag1.jpg",
+"images/bag2.jpg"
+]
+}
+
+];
